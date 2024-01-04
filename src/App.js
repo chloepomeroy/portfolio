d43d5296cde7d05d16817fd2c120e44847299e3e
chloepomeroy <pomeroyc@outlook.com>
2024-01-04 00:24:59 -0500

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, HashRouter } from "react-router-dom";
 import "./App.css";
-// import NavBar from "./components/NavBar";
-// import Banner from "./components/Banner";
-// import Projects from "./components/Projects";
-// import Contact from "./components/Contact";
-// import Layout from "./components/Layout";
+import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={"http://chloepomeroy.github.io/portfolio"}>
+      <HashRouter basename={"http://chloepomeroy.github.io/portfolio"}>
         <Routes>
           <Route path="/" element={<Layout />}>
-            {/* <Route index element={<Banner />} />
+            <Route index element={<Banner />} />
             <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} /> */}
+            <Route path="/contact" element={<Contact />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
