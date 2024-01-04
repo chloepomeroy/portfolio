c2aa1fd29e20b8784d98a4c22c3f3284c847278b
chloepomeroy <pomeroyc@outlook.com>
2024-01-04 00:18:40 -0500

    test

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Banner from "./components/Banner";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Layout from "./components/Layout";
+// import NavBar from "./components/NavBar";
+// import Banner from "./components/Banner";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
+// import Layout from "./components/Layout";
 
 function App() {
   return (
