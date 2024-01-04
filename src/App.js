4fadd829a08ac3ccbae56ce12d39731402e8c645
chloepomeroy <pomeroyc@outlook.com>
2024-01-04 00:36:24 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,67 +1,27 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { Routes, Route, HashRouter } from "react-router-dom";
-// import "./App.css";
-// import NavBar from "./components/NavBar";
-// import Banner from "./components/Banner";
-// import Projects from "./components/Projects";
-// import Contact from "./components/Contact";
-// import Layout from "./components/Layout";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, HashRouter } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Layout from "./components/Layout";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <HashRouter basename={"http://chloepomeroy.github.io/portfolio"}>
-//         <Routes>
-//           <Route path="/" element={<Layout />}>
-//             <Route index element={<Banner />} />
-//             <Route path="/projects" element={<Projects />} />
-//             <Route path="/contact" element={<Contact />} />
-//           </Route>
-//         </Routes>
-//       </HashRouter>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import './App.css';
-import React from 'react';
-import { Route, Switch, Link } from "react-router-dom";
-import About from './components/Contact';
-import Home from './components/Layout';
-
-class App extends React.Component {
-  render() {
-      return (
-        <div className="App">
-          <div>
-            <nav>
-              <ul id="navigation">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                <Link to="/about">About</Link>
-                </li>
-                <li>
-                <Link to="/contact">Contact</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-            <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-          </Switch>
-          </div>
-            );
-  }
+function App() {
+  return (
+    <div className="App">
+      <HashRouter basename={"http://chloepomeroy.github.io/portfolio"}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Banner />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </div>
+  );
 }
 
 export default App;
