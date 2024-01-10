bd74532363df1ea9f8c34c0c24d1fc36f0cf0f60
Chloe Pomeroy <pomeroyc@outlook.com>
2024-01-09 19:16:15 -0500

    fixing layout for certifications

--- src/components/Pages/Certifications.js
+++ src/components/Pages/Certifications.js
@@ -10,30 +10,30 @@ import comptiaDataPlus from "../../assets/dataPlusCert.png"
 export default function Certifications() {
 
     return (
-        <Box sx={{ width: "100%", justifyContent: "center" }}>
-            <Grid container spacing={2} padding={2} sx={{ justifyContent: "center" }}>
-                <Grid item xs={10} style={{ textAlign: "left" }}>
-                    <PageTitle pageTitle="Certifications" />
-                </Grid>
-                <Grid item xs={10} style={{ textAlign: "left" }}>
-                    <Button variant="outlined" href="https://www.credly.com/users/chloe-pomeroy">See All on Credly</Button>
-                </Grid>
+        // <Box sx={{ width: "100%", justifyContent: "center" }}>
+        <Grid container spacing={2} padding={2} sx={{ justifyContent: "center" }}>
+            <Grid item xs={10} style={{ textAlign: "left" }}>
+                <PageTitle pageTitle="Certifications" />
+            </Grid>
+            <Grid item xs={10} marginBottom={2} style={{ textAlign: "left" }}>
+                <Button variant="outlined" href="https://www.credly.com/users/chloe-pomeroy">See All on Credly</Button>
             </Grid>
 
-            <Grid container xs={10} spacing={1} padding={2} sx={{ justifyContent: "center" }}>
-                <Grid item xs={6} md={4} style={{ display: 'flex', justifyContent: "center" }}>
+            <Grid container xs={10} spacing={2} padding={2} >
+                {/* <Grid container xs={10} spacing={1} padding={2} sx={{ justifyContent: "center" }}> */}
+                <Grid item xs={6} md={4} style={{ display: 'flex', justifyContent: "left" }}>
                     <img src={azureDataSci} width="400" height="400" />
                 </Grid>
 
-                <Grid item xs={6} md={4} style={{ display: 'flex', justifyContent: "center" }}>
+                <Grid item xs={6} md={4} style={{ display: 'flex', justifyContent: "left" }}>
                     <img src={azureDataEng} width="400" height="400" />
                 </Grid>
 
-                <Grid item xs={6} md={4} style={{ display: 'flex', justifyContent: "center" }}>
+                <Grid item xs={6} md={4} style={{ display: 'flex', justifyContent: "left" }}>
                     <img src={comptiaDataPlus} width="400" height="400" />
                 </Grid>
             </Grid>
-        </Box>
+        </Grid>
     )
 
 }
